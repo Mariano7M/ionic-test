88f3a69021b1f3c68d0533e6f13bec6a51e327cf
Marinho <swipeappsteam@gmail.com>
2020-01-23 22:17:40 -0600

    Added some logic to tab2.page component

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Dto } from '../models/dto';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  public title: string;
+  public list: Dto[];
+  constructor(public alertController: AlertController) {
+    this.title = 'List example';
+    this.list = [
+      {
+        title: 'Title 1',
+        subTitle: 'Subtitle 1',
+        img: 'assets/img/batman-logo-01.jpg',
+        text: 'Some text ramdom'
+      },
+      {
+        title: 'Title 2',
+        subTitle: 'Subtitle 2',
+        img: 'assets/img/batman-logo-02.png',
+        text: 'some text ramdom'
+      },
+      {
+        title: 'Title 3',
+        subTitle: 'Subtitle 3',
+        img: 'assets/img/batman-logo-03.png',
+        text: 'some text ramdon'
+      }
+    ];
+  }
 
-  constructor() {}
+  async showAlert(dto: Dto) {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      message: dto.text,
+      buttons: ['OK']
+    });
 
+    await alert.present();
+  }
 }
